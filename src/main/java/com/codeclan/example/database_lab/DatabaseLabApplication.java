a5f93be07650a0e6d9755d433ca1009603136898
Martin <sileshs@gmail.com>
2019-04-09 09:58:39 +0100

    creating initial repo

---
+++ src/main/java/com/codeclan/example/database_lab/DatabaseLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.database_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseLabApplication.class, args);
+	}
+
+}
